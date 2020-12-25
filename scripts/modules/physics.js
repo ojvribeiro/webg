eb11131f35e2a4e81be148ef1831a26a46af9868
Victor Ribeiro <devjvribeiro@gmail.com>
2020-12-25 14:34:06 -0300

    Created rectangle-rectangle collision resolution

--- scripts/modules/physics.js
+++ scripts/modules/physics.js
@@ -3,6 +3,44 @@ import { State } from './states.js';
 
 const Physics = {
 
+  collision: {
+    rectRect: (rectA, rectB) => {
+      // Get the vectors to check against
+      const vectorX = (rectA.x + (rectA.width / 2)) - (rectB.x + (rectB.width / 2));
+      const vectorY = (rectA.y + (rectA.height / 2)) - (rectB.y + (rectB.height / 2));
+
+      // Half widths and half heights of the objects
+      const ABMiddleHorizontal = (rectA.width / 2) + (rectB.width / 2);
+      const ABMiddleVertical = (rectA.height / 2) + (rectB.height / 2);
+    
+      // If the x and y vector are less than the half width or half height,
+      // They we must be inside the object, causing a collision
+      if (Math.abs(vectorX) < ABMiddleHorizontal && Math.abs(vectorY) < ABMiddleVertical) {
+        // Figures out on which side we are colliding (top, bottom, left, or right)
+        const oX = ABMiddleHorizontal - Math.abs(vectorX);
+        const oY = ABMiddleVertical - Math.abs(vectorY);
+
+        if (oX >= oY) {
+          if (vectorY > 0) {
+            State.player.y += oY;
+          }
+          else {
+            State.player.y -= oY;
+          }
+        }
+        else {
+          if (vectorX > 0) {
+            State.player.x += oX;
+          }
+          else {
+            State.player.x -= oX;
+          }
+        }
+      }
+    },
+  },
+
+
   velocity: function(angle, speed) {
     return {
       x: Math.cos(angle) * speed,
